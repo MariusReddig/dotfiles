1ff3b69c87a228a99cb2c3f7efc8d0771bc50841
MariusReddig <96890272+MariusReddig@users.noreply.github.com>
2025-03-11 20:06:36 +0100

    feat: add rocm environment

--- nixos/core/drivers/amd.nix
+++ nixos/core/drivers/amd.nix
@@ -17,4 +17,19 @@
         clinfo
       ];
     };
+
+    systemd.tmpfiles.rules =
+      let
+        rocmEnv = pkgs.symlinkJoin {
+          name = "rocm-combined";
+          paths = with pkgs.rocmPackages; [
+            rocblas
+            hipblas
+            clr
+          ];
+        };
+      in
+    [
+      "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+    ];
 }
