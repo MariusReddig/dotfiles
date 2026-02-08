357a3a9134dc476ac0d90999038074afc7f6144f
MariusReddig <96890272+MariusReddig@users.noreply.github.com>
2026-02-08 23:29:12 +0100

    feat(home): update superfile to 1.5.0

--- home/superfile/superfile.nix
+++ home/superfile/superfile.nix
@@ -1,7 +1,15 @@
-{ pkgs, inputs, config, ... }: {
+{ pkgs, inputs, config, ... }:
+let
+  superfile =
+    inputs.superfile.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  superfileWithoutChecks = superfile.overrideAttrs (old: {
+    doCheck = false;
+    doInstallCheck = false;
+  });
+in {
   programs.superfile = {
     enable = true;
-    # package = inputs.superfile.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    package = superfileWithoutChecks;
     metadataPackage = pkgs.unstable.exiftool;
     zoxidePackage = pkgs.unstable.zoxide;
     # settings = builtins.fromTOML (builtins.readFile ./config.toml);
