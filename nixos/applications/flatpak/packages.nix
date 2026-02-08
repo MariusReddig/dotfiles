d9b2409de1389253c4655f38de6a9ae4997d01eb
MariusReddig <96890272+MariusReddig@users.noreply.github.com>
2026-02-08 23:30:08 +0100

    feat(home): add soundux flatpak-package

--- nixos/applications/flatpak/packages.nix
+++ nixos/applications/flatpak/packages.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }: {
   services.flatpak.enable = true;
-  services.flatpak.packages = [{
-    appId = "com.stremio.Stremio";
-    origin = "flathub";
-  }];
+  services.flatpak.packages = [
+    {
+      appId = "com.stremio.Stremio";
+      origin = "flathub";
+    }
+    {
+      appId = "io.github.Soundux";
+      origin = "flathub";
+    }
+  ];
 
   # Configure XDG portals (important for Hyprland)
   xdg.portal = {
