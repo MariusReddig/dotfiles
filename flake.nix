51461d8b745d6228833511f86c9da89d3b99250e
MariusReddig <96890272+MariusReddig@users.noreply.github.com>
2026-04-22 14:29:08 +0200

    nix(flake): update flake

--- flake.nix
+++ flake.nix
@@ -123,6 +123,7 @@
             (coreModule "nix-ld")
             (coreModule "docker")
             (coreModule "battery-monitor")
+            (coreModule "cups")
 
             # Host configuration
             ./hosts/laptop/configuration.nix
